5618e61bbde1faacadc73b605542231e4fc07a7b
RobertoSimonini <robertosimonini1@gmail.com>
2023-02-14 15:27:48 +0100

    3rd task done

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -14,6 +14,9 @@ WHERE `cfu` > 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
+SELECT `name`, `surname`, `date_of_birth`
+FROM `students`
+WHERE `date_of_birth` <= '1992';
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
