5c48805964e216a79d366fae4e8ec9c975cc42ed
RobertoSimonini <robertosimonini1@gmail.com>
2023-02-14 15:18:57 +0100

    first task done

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -2,6 +2,9 @@
 
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
 
+SELECT `name`, `surname`, `date_of_birth`
+FROM `students`
+WHERE `date_of_birth` BETWEEN '1990-01-01' AND '1990-12-31';
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 
