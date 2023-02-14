727eec6bbfd2755a23a57e62345bf4d88955ae4c
RobertoSimonini <robertosimonini1@gmail.com>
2023-02-14 15:34:53 +0100

    4th task done

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -20,6 +20,10 @@ WHERE `date_of_birth` <= '1992';
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
+SELECT `name`, `period`, `year`
+FROM `courses`
+WHERE `year` = 1
+AND `period` = 'I semestre';
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
